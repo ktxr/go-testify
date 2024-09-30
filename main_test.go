afdccec1d1214ebbfade989952f5627fa42993b6
Ivan Lebedenko <imlnl@icloud.com>
2024-09-30 15:25:39 +0300

    update test

--- main_test.go
+++ main_test.go
@@ -3,9 +3,11 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMainHandlerWhenOk(t *testing.T) {
@@ -16,7 +18,7 @@ func TestMainHandlerWhenOk(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.Equal(t, responseRecorder.Code, http.StatusOK)
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
 	assert.NotEmpty(t, responseRecorder.Body)
 }
 
@@ -28,7 +30,7 @@ func TestMainHandlerWhenWrongCity(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
+	require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
 
 	expectedAnswer := "wrong city value"
 	assert.Equal(t, responseRecorder.Body.String(), expectedAnswer)
@@ -42,6 +44,9 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.Equal(t, responseRecorder.Code, http.StatusOK)
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
 	assert.NotEmpty(t, responseRecorder.Body.String())
+
+	actualCount := len(strings.Split(responseRecorder.Body.String(), ","))
+	assert.Equal(t, actualCount, 4)
 }
